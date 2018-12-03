832a4b1cb6ebfcdd07e266cff1da0ea6cf77e07e
18020777 <45566880+18020777@users.noreply.github.com>
2018-12-03 16:10:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,8 @@
-/**********************************************************************/
-/*******            Projet d'algo prog struct 2018-2019   *************/
-/**********************************************************************/
+#include "Header.h"
 
-/*********** 1) Zone des #include ***************************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-
-/************ 2) ci-dessous les #define *********************/
-#define L_FENETRE 500
-#define H_FENETRE 460
-
-#define CASE_SIZE 50 
-#define NB_CASE_X	5
-#define NB_CASE_Y	9
-
-#define BOULE_SIZE 20
-
-/********** 3) puis la ou les définitions des structures ***/
-struct sCase
-{
-	int x, y, valeur;								// valeur = 0 (pas de case)  = 1 (présence d'une case)
-};
-typedef struct sCase Case;
-
-
-/********** 4) les prototypes des fonctions  ***************/
-void dessineScene(SDL_Renderer *rendu, SDL_Texture *texture);
-void dessineMat(SDL_Renderer *rendu, SDL_Texture *texture);
-void dessineCase(int lig, int col, SDL_Renderer *rendu, SDL_Texture *texture);
-Case initMatrice();
-
-/************et enfin les variables globales************/
-Case matrice[NB_CASE_X][NB_CASE_Y];
-
-/***************  La fonction main()    *********************/
 int main(int argc, char** argv)
 {
-	/*********************Déclaration de toutes les variables ***********************/
+	/*********************DÃ©claration de toutes les variables ***********************/
 	SDL_Window *mainFenetre = NULL;
 	SDL_Renderer *rendu = NULL;
 	SDL_Event event;
@@ -47,36 +10,28 @@ int main(int argc, char** argv)
 
 	int quit = 0;
 
-	
-
-
-
-	/*********************Démarrage de la SDL ***********************/
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0){printf("\nImpossible d initialiser SDL :  %s\n", SDL_GetError());	exit(EXIT_FAILURE);	}
 
-	/*********************Création de la Fenetre principale ***********************/
+
 	mainFenetre = SDL_CreateWindow("Mon Super Programme", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, L_FENETRE, H_FENETRE, SDL_WINDOW_SHOWN);             // flags - see below
 	if (mainFenetre == NULL){printf("\nImpossible de creer la fenetre :  %s\n", SDL_GetError());         exit(EXIT_FAILURE);	}	rendu = SDL_CreateRenderer(mainFenetre, -1, SDL_RENDERER_ACCELERATED);
 	if (rendu == NULL){	printf("\nImpossible de creer le rendu :  %s\n", SDL_GetError());         exit(EXIT_FAILURE);	}
-	SDL_SetRenderDrawColor(rendu, 0, 0, 220, 255); //la fenêtre est bleue
+	SDL_SetRenderDrawColor(rendu, 0, 0, 220, 255); //la fenÃªtre est bleue
 
 	initMatrice();
 	dessineMat(rendu, texture);
 
-
-
-	/********************* Chargement des fichiers images pour la création des textures ***********/
 	texture = IMG_LoadTexture(rendu, "./images/case_1.png");
 	if (texture == NULL){printf("\nProbleme sur la texture\n");         exit(EXIT_FAILURE);	}
 
 
-	///********************* La boucle des événements ******************************/
-	while (!quit)  //que l'on pourrait écrire quit==0
+	///********************* La boucle des Ã©vÃ©nements ******************************/
+	while (!quit)  //que l'on pourrait Ã©crire quit==0
 	{
 		SDL_PollEvent(&event);
 		switch (event.type)
 		{
-		case SDL_QUIT: //clic sur la croix en haut à droite
+		case SDL_QUIT: //clic sur la croix en haut Ã  droite
 			quit = 1;
 			break;
 
@@ -87,16 +42,16 @@ int main(int argc, char** argv)
 				quit = 1; // on quitte
 				break;
 
-			case SDLK_LEFT:   //la touche flèche gauche
+			case SDLK_LEFT:   //la touche flÃ¨che gauche
 
 				break;
 
-			case SDLK_RIGHT:   //la touche flèche gauche
+			case SDLK_RIGHT:   //la touche flÃ¨che gauche
 
 				break;
 
 			case SDLK_d: //la touche d
-				printf("on a appuyé sur la touche d\n");
+				printf("on a appuyÃ© sur la touche d\n");
 				break;
 			}
 			break;
@@ -112,12 +67,12 @@ int main(int argc, char** argv)
 				printf("clic droit de souris\n");
 				break;
 			}
-			event.button.button = 0; // on ré initialise l'événement
+			event.button.button = 0; // on rÃ© initialise l'Ã©vÃ©nement
 			break;
 		}
 		/********************* Appel de la fonction dessineScene() *********************/
 		dessineScene(rendu, texture);
-		SDL_Delay(30);  //Attente de 30 ms avant de gérer le prochain événement
+		SDL_Delay(30);  //Attente de 30 ms avant de gÃ©rer le prochain Ã©vÃ©nement
 	}
 
 	/*********************FIN du programme destruction des textures, du rendu et Fin de la SDL*******************/
@@ -157,7 +112,7 @@ void dessineScene(SDL_Renderer *rendu, SDL_Texture *texture)
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 	SDL_RenderFillRect(rendu, &dest_airejeu);
 
-	// avec peut-être à l'interieur des images
+	// avec peut-Ãªtre Ã  l'interieur des images
 	SDL_RenderCopy(rendu, texture, NULL, &dest_image);
 
 
@@ -210,6 +165,3 @@ void dessineMat(SDL_Renderer *rendu, SDL_Texture *texture)
 		}
 	}
 }
-
-
-//"./images/case_1.png"
