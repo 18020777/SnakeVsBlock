8363b8e2238e08541670a9ed8963c4e0cd80e57a
Lubra13 <45567179+Lubra13@users.noreply.github.com>
2018-12-04 09:37:17 +0100

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -1,55 +1,98 @@
 #include "Header.h"
 
+extern Case matrice[NB_CASE_Y][NB_CASE_X];
+
+void initMat(void)
+{
+	int i, j;
+	for (i = 0; i < NB_CASE_Y; i++)
+	{
+		for (j = 0; j < NB_CASE_X; j++)
+		{
+			matrice[i][j].y =i * CASE_SIZE;
+			matrice[i][j].x = j * CASE_SIZE;
+			matrice[i][j].valeur = 0;
+		}
+	}
+}
+//-------------------------------------------------------------
+void dessineCase(int j, int i, SDL_Renderer*rendu, SDL_Texture*texture)
+{
+
+	SDL_Rect r_case;
+	r_case.x = matrice[i][j].x;
+	r_case.y = matrice[i][j].y;
+	r_case.h = CASE_SIZE;
+	r_case.w = CASE_SIZE;
+	//SDL_SetRenderDrawColor(rendu, 255, 0, 0, 255);
+	texture = IMG_LoadTexture(rendu, "./images/case_1.png");
+	SDL_RenderCopy(rendu, texture, NULL, &r_case);
+	//SDL_RenderFillRect(rendu, &r_case);
+}
+
+//----------------------------------------------------------------------------
+void dessineMat(SDL_Renderer *rendu, SDL_Texture *texture)
+{
+	int i, j;
+	for (i = 0; i < NB_CASE_Y; i++)
+	{
+		for (j = 0; j < NB_CASE_X; j++)
+		{
+			if (matrice[i][j].valeur == 1)
+			{
+				dessineCase(j,i,rendu,texture);
+			}		
+		}
+	}
+}
+
+//--------------------------------------------------------------------------
+
+//-------------------------------------------------------------
+
 void dessineScene(SDL_Renderer *rendu, SDL_Texture *texture)
 {
-	Case **matrix = NULL;
-	matrix = ((Case**)malloc(NB_CASE_X * sizeof(Case)));
-	int i;
-	for (i = 0; i < NB_CASE_X; i++)
-	{
-		matrix[i] = (Case*)malloc(NB_CASE_Y * sizeof(Case));
-	}
-	initMatrix(matrix);
 	static SDL_Rect dest_fenetre = { 0, 0, L_FENETRE, H_FENETRE };
-	static SDL_Rect dest_airejeu = { 20, 20, CASE_SIZE*NB_CASE_X,  CASE_SIZE * (NB_CASE_Y - 1) };
+	static SDL_Rect dest_airejeu = { 0, 0, CASE_SIZE*NB_CASE_X,  CASE_SIZE * (NB_CASE_Y - 1) };
 	static SDL_Rect dest_image = { CASE_SIZE*NB_CASE_X / 2, CASE_SIZE*(NB_CASE_Y - 1) / 2, CASE_SIZE, CASE_SIZE };
-	SDL_Rect dest_case = { matrix[3][1].x, matrix[1][1].y , CASE_SIZE, CASE_SIZE };
 	/****************On efface la scene **************************/
 	SDL_RenderClear(rendu);
 
 	/****************On dessine la scene ************************/
 	// d'abord le fond
-	SDL_SetRenderDrawColor(rendu, 0, 0, 255, 255);
+	//SDL_SetRenderDrawColor(rendu, 0, 0, 255, 255);
 	SDL_RenderFillRect(rendu, &dest_fenetre);
 
 	// ensuite l'aire de jeu sous la forme d'un rectangle noir de 5 par 8 cases
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 	SDL_RenderFillRect(rendu, &dest_airejeu);
-	SDL_SetRenderDrawColor(rendu, 0, 255, 0, 0);
-	SDL_RenderFillRect(rendu, &dest_case);
-	// avec peut-être à l'interieur des images
+
+	// avec peut-Ãªtre Ã  l'interieur des images
+	dessineMat(rendu, texture);
+	SDL_SetRenderDrawColor(rendu, 0, 0, 255, 255);
 	SDL_RenderCopy(rendu, texture, NULL, &dest_image);
 
+
 	/****************On presente la scene ************************/
-	SDL_RenderPresent(rendu);  //ensuite on affiche la nouvelle
+	
+
 }
 
-void initMatrix(Case **matmat)
+void scrollMat(void)
 {
-	matmat = ((Case**)malloc(NB_CASE_X * sizeof(Case)));
-	int i;
-	for (i = 0; i < NB_CASE_X; i++)
+	int i, j;
+	for (i = 0; i < NB_CASE_Y-1; i++)
 	{
-		matmat[i] = (Case*)malloc(NB_CASE_Y * sizeof(Case));
-	}
-	int cntX, cntY;
-	for (cntY = 0; cntY < NB_CASE_Y; cntY++)
-	{
-		for (cntX = 0; cntX < NB_CASE_X; cntX++)
+		for (j = 0; j < NB_CASE_X; j++)
 		{
-			matmat[cntX][cntY].x = cntX * CASE_SIZE;
-			matmat[cntX][cntY].y = (cntY+1) * CASE_SIZE;
-			matmat[cntX][cntY].val = 0;
+			matrice[i][j].y++;
+			if (matrice[i][j].y > 390)
+			{
+				matrice[i][j].y = -50;
+				matrice[i][j].valeur = rand() % 4;
+			}
 		}
 	}
+	
+	
 }
